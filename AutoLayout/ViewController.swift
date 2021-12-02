f5d1b0d1eb68fee0549b9496aad28dc1a121283a
Jorge Roberto <jorgercj@dcomp.ufs.br>
2021-12-01 21:38:12 -0300

    Secont example using width, height and center anchors

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -11,17 +11,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-         
-         let redView = UIView()
-         view.addSubview(redView)
-         
-         redView.backgroundColor = .red
-         redView.translatesAutoresizingMaskIntoConstraints = false
-         
-         redView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
-         redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-         redView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50).isActive = true
-         redView.bottomAnchor .constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+        
+        
+        let redView = UIView()
+        view.addSubview(redView)
+        redView.backgroundColor = .red
+        redView.translatesAutoresizingMaskIntoConstraints = false
+        
+        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         
     }
