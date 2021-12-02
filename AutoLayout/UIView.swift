b076816b40ab954e2da207d79e84f26df5ff54bf
Jorge Roberto <jorgercj@dcomp.ufs.br>
2021-12-01 22:12:46 -0300

    refactoring code to help create subViews

---
+++ AutoLayout/UIView.swift
@@ -0,0 +1,31 @@
+//
+//  UIView.swift
+//  AutoLayout
+//
+//  Created by Jorge de Carvalho on 01/12/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func fill(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, trailing: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, padding: UIEdgeInsets = .zero)  {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+         
+    }
+}
--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -16,27 +16,19 @@ class ViewController: UIViewController {
         view.addSubview(redView)
         redView.backgroundColor = .red
         
-        let blueView = UIView()
-        view.addSubview(blueView)
-        blueView.backgroundColor = .blue
-        
-        let yellowView = UIView()
-        view.addSubview(yellowView)
-        yellowView.backgroundColor = .yellow
-        
-        let horizontalStackView: UIStackView = UIStackView(arrangedSubviews: [redView, blueView])
-        let stackView: UIStackView = UIStackView(arrangedSubviews: [horizontalStackView, yellowView])
-        
-        view.addSubview(stackView)
-        stackView.distribution = .fillEqually
-        horizontalStackView.distribution = .fillEqually
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-        
-        stackView.axis = .vertical
+        redView.fill(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            trailing: view.trailingAnchor,
+            bottom: view.bottomAnchor,
+            padding:
+                    .init(
+                        top: 50,
+                        left: 30,
+                        bottom: 100,
+                        right: 30
+                    )
+        )
     }
     
     
