bcab40d935a6a712e52505ee5736799801a803a8
Jorge Roberto <jorgercj@dcomp.ufs.br>
2021-12-01 21:39:22 -0300

    Using two subviews for horizontal align

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -12,17 +12,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
         let redView = UIView()
         view.addSubview(redView)
         redView.backgroundColor = .red
+        
+        let blueView = UIView()
+        view.addSubview(blueView)
+        blueView.backgroundColor = .blue
+        
         redView.translatesAutoresizingMaskIntoConstraints = false
+        redView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        redView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        //redView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        redView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        redView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        redView.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        redView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        redView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        redView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
         
+        
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        blueView.leadingAnchor.constraint(equalTo: redView.trailingAnchor).isActive = true
+        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
     }
     
